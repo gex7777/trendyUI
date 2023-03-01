55128ecb85d3ddcca37d19c5566d50ba6f3da0f5
George <gex7777@gmail.com>
2023-03-01 23:18:18 +0530

    add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: " #14172B",
+        "border-gradient-top": "#303448",
+        "border-gradient-bottom": "#ECF0FF1A",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
