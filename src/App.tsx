c0999d3b6b752922fdcfaae820745ded4a0c8c1f
George <gex7777@gmail.com>
2023-03-01 23:21:13 +0530

    create canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Card } from "./components/Card";
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-background">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
