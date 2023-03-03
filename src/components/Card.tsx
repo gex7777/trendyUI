63039f5c1b2559e3288392260fcd194889b6d6ec
George <gex7777@gmail.com>
2023-03-03 21:35:36 +0530

    add flex boxes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Card = () => {
   return (
-    <div className="card-linear-br rounded-2xl w-72 h-96 ">
+    <div className="card-linear-br flex flex-col justify-center items-center rounded-2xl w-72 h-96 ">
       <div className="icon rounded-b-full h-[60.5px] w-[121px]  relative mx-auto  border border-border-gradient-top bg-background border-t-0 ">
         {" "}
         <div
@@ -37,40 +37,45 @@ bg-[#1b1f34]
           />
         </div>
       </div>
-      <div className="description w-full flex flex-col justify-center items-center">
+      <div className="description w-full  h-full flex flex-col justify-evenly items-center">
         <div className="title text-slate-500">Shibu Inu</div>
-        <div className="description-box-linear-br w-9/12 h-12 grid place-items-center rounded-full relative">
-          <div className="percentage-price text-indigo-50">3244423423</div>
-          <div className="percentage-difference absolute right-0 text-red-500 ">
-            {" "}
-            -6.00%
+        <div className="w-full grid place-items-center">
+          <div className="description-box-linear-br w-9/12 h-12 grid place-items-center rounded-full relative">
+            <div className="percentage-price text-indigo-50">3244423423</div>
+            <div className="percentage-difference absolute right-0 text-red-500 ">
+              -6.00%
+            </div>
           </div>
+          <div className="price text-gray-500">Price</div>
         </div>
-        <div className="price text-gray-500">Price</div>
-        <div className="description-box-linear-br w-9/12 h-12 grid place-items-center rounded-full relative">
-          <div className="percentage-price text-indigo-50">3244423423</div>
+        <div className="w-full grid place-items-center">
+          <div className="description-box-linear-br w-9/12 h-12 grid place-items-center rounded-full relative">
+            <div className="percentage-price text-indigo-50">3244423423</div>
+          </div>
+          <div className="tvl text-gray-500">TVL</div>
         </div>
-        <div className="tvl text-gray-500">TVL</div>
-        <div className="popular-box bg-background px-4 py-2 rounded-full flex justify-evenly items-center gap-4">
-          <img
-            src="src/assets/icons/SHIBA INU (SHIB).svg"
-            className="w-[20px]  aspect-square"
-            alt=""
-          />
+        <div className="w-full grid place-items-center">
+          <div className="popular-box bg-background px-4 py-2 rounded-full flex justify-evenly items-center gap-4">
+            <img
+              src="src/assets/icons/SHIBA INU (SHIB).svg"
+              className="w-[20px]  aspect-square"
+              alt=""
+            />
 
-          <img
-            src="src/assets/icons/SHIBA INU (SHIB).svg"
-            className="w-[20px] aspect-square"
-            alt=""
-          />
+            <img
+              src="src/assets/icons/SHIBA INU (SHIB).svg"
+              className="w-[20px] aspect-square"
+              alt=""
+            />
 
-          <img
-            src="src/assets/icons/SHIBA INU (SHIB).svg"
-            className="w-[20px] aspect-square"
-            alt=""
-          />
+            <img
+              src="src/assets/icons/SHIBA INU (SHIB).svg"
+              className="w-[20px] aspect-square"
+              alt=""
+            />
+          </div>
+          <div className="popular text-gray-500">Popular pairs</div>
         </div>
-        <div className="popular text-gray-500">Popular pairs</div>
       </div>
     </div>
   );
