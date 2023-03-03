5baca6cc134be334f2846a98d0d306e43a2c78bf
George <gex7777@gmail.com>
2023-03-03 20:54:27 +0530

    add decription box

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,25 +6,25 @@ export const Card = () => {
       <div className="icon rounded-b-full h-[60.5px] w-[121px]  relative mx-auto  border border-border-gradient-top bg-background border-t-0 ">
         {" "}
         <div
-          className="patch-r  absolute  w-2
+          className="patch-l  absolute  w-2
     h-[16px]
 
     top-[21px]
     left-[2px]
   origin-bottom
-   skew-x-[27deg]
+   skew-x-[25deg]
     z-1
     bg-[#1b1f34]
 }"
         ></div>
         <div
-          className="patch-l absolute  w-2
+          className="patch-r absolute  w-2
     h-[15px]
 bg-[#1b1f34]
     top-[21px]
-    right-[1px]
+    right-[2px]
   origin-bottom
-   skew-x-[157deg]
+   skew-x-[153deg]
     z-10
     
 }"
@@ -36,6 +36,41 @@ bg-[#1b1f34]
             alt=""
           />
         </div>
+      </div>
+      <div className="description w-full flex flex-col justify-center items-center">
+        <div className="title text-slate-500">Shibu Inu</div>
+        <div className="description-box-linear-br w-9/12 h-12 grid place-items-center rounded-full relative">
+          <div className="percentage-price text-indigo-50">3244423423</div>
+          <div className="percentage-difference absolute right-0 text-red-500 ">
+            {" "}
+            -6.00%
+          </div>
+        </div>
+        <div className="price text-gray-500">Price</div>
+        <div className="description-box-linear-br w-9/12 h-12 grid place-items-center rounded-full relative">
+          <div className="percentage-price text-indigo-50">3244423423</div>
+        </div>
+        <div className="tvl text-gray-500">TVL</div>
+        <div className="popular-box bg-background px-4 py-2 rounded-full flex justify-evenly items-center gap-4">
+          <img
+            src="src/assets/icons/SHIBA INU (SHIB).svg"
+            className="w-[20px]  aspect-square"
+            alt=""
+          />
+
+          <img
+            src="src/assets/icons/SHIBA INU (SHIB).svg"
+            className="w-[20px] aspect-square"
+            alt=""
+          />
+
+          <img
+            src="src/assets/icons/SHIBA INU (SHIB).svg"
+            className="w-[20px] aspect-square"
+            alt=""
+          />
+        </div>
+        <div className="popular text-gray-500">Popular pairs</div>
       </div>
     </div>
   );
