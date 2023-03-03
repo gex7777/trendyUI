b362525583500c4d5d74e1b49b2433ab9d52053c
George <gex7777@gmail.com>
2023-03-03 14:01:03 +0530

    small tweaks

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export const Card = () => {
   return (
-    <div className="linear-br rounded-2xl w-72 h-96 ">
+    <div className="card-linear-br rounded-2xl w-72 h-96 ">
       <div className="icon rounded-b-full h-[60.5px] w-[121px]  relative mx-auto  border border-border-gradient-top bg-background border-t-0 ">
         {" "}
         <div
           className="patch-r  absolute  w-2
     h-[13px]
 
-    top-[24px]
+    top-[21px]
     left-[2px]
   origin-bottom
    skew-x-[27deg]
@@ -22,14 +22,20 @@ export const Card = () => {
     h-[15px]
 bg-[#1a1e33]
     top-[21px]
-    left-[109px]
+    right-[2px]
   origin-bottom
    skew-x-[153deg]
     z-10
     
 }"
         ></div>
-        <div className="circle aspect-square h-[100px] bg-red-500 relative mx-auto z-20 rounded-full translate-y-[-50%] "></div>
+        <div className="shib-linear-br grid place-items-center aspect-square h-[100px] relative mx-auto z-20 rounded-full translate-y-[-50%] ">
+          <img
+            src="src/assets/icons/SHIBA INU (SHIB).svg"
+            className="w-[50px] aspect-square"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
